2e2cc85292d073c49a998b59d326cf35b0029c58
Shajan Jacob <shajanjp@gmail.com>
2015-11-22 13:14:24 +0530

    Adds input files for 1a and 1b

---
+++ BE/VTU/5/Lab/SS-OS-Unix/source.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+/* All set
+   Lets get to work */
+main(){
+	//Decraring variables
+	int a,b,c;
+	//Therse are of no use.
+	float d,e;
+	int f,g;
+	/* The main part of the program
+	   Everything happens here */
+	printf("Hello world is coming back !");
+	//Last part of the program. Dont be sad.
+}
