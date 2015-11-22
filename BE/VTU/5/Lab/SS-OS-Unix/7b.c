c127fc4e6d716328358a418e696927278cccfe6e
Shajan Jacob <shajanjp@gmail.com>
2015-11-22 15:28:45 +0530

    Adds program 7b

---
+++ BE/VTU/5/Lab/SS-OS-Unix/7b.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+#include<sys/types.h>
+main()
+{
+    char cmd[10];
+    int ch;
+    pid_t pid;
+    pid=fork();
+    if(pid==0)
+    {
+        do
+        {
+            printf("Enter the command to be executed:\n");
+            scanf("%s",cmd);
+            system(cmd);
+            printf("\n Enter 1 to enter more commands,0 to exit\n");
+            scanf("%d",&ch);
+        }while(ch!=0);
+    }
+wait();
+}
