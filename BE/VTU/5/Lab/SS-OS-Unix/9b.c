2f0a19073e8f213445cd6d440fab35cff368b35a
Shajan Jacob <shajanjp@gmail.com>
2015-11-24 22:05:11 +0530

    Indents code

--- BE/VTU/5/Lab/SS-OS-Unix/9b.c
+++ BE/VTU/5/Lab/SS-OS-Unix/9b.c
@@ -1,20 +1,20 @@
 #include<stdio.h>
 main()
 {
-int pid;
-pid=fork();
-if(pid==0)
-{
-printf("********child's process id *****\n");
-printf("Child process id = %d \n",getpid());
-printf("Parent process id = %d \n",getppid());
-}
-else
-{
-wait();
-printf("****** parent's process id *******\n");
-printf("Parent process id = %d \n",getpid());
-printf("Child's process id = %d " ,pid);
-}
-return 0;
+	int pid;
+	pid=fork();
+	if(pid==0)
+	{
+		printf("********child's process id *****\n");
+		printf("Child process id = %d \n",getpid());
+		printf("Parent process id = %d \n",getppid());
+	}
+	else
+	{
+		wait();
+		printf("****** parent's process id *******\n");
+		printf("Parent process id = %d \n",getpid());
+		printf("Child's process id = %d " ,pid);
+	}
+	return 0;
 }
