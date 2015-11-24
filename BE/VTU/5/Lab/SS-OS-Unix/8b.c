75324099891713f27dc9510ce5fad064c4df2545
Shajan Jacob <shajanjp@gmail.com>
2015-11-24 21:47:25 +0530

    Indents code

--- BE/VTU/5/Lab/SS-OS-Unix/8b.c
+++ BE/VTU/5/Lab/SS-OS-Unix/8b.c
@@ -6,16 +6,16 @@
 #include<stdio.h>
 main()
 {
-int fd;
-char buf1[]="abcdefghijklmnopq";
-char buf2[]="ABCDEFGHIJKLMNOPQ";
-fd=creat("textfile",0622);
-if(fd<0)
-{
-printf("file is not created\n");
-exit(0);
-}
-write(fd,buf1,16);
-lseek(fd,48,SEEK_SET);
-write(fd,buf2,16);
+	int fd;
+	char buf1[]="abcdefghijklmnopq";
+	char buf2[]="ABCDEFGHIJKLMNOPQ";
+	fd=creat("textfile",0622);
+	if(fd<0)
+	{
+		printf("file is not created\n");
+		exit(0);
+	}
+	write(fd,buf1,16);
+	lseek(fd,48,SEEK_SET);
+	write(fd,buf2,16);
 }
