5e4383c9ffdf0f40d72d81741a7b23c9c80e96ce
shajanjp <shajanjp@gmail.com>
2016-05-26 22:48:09 +0530

    Removes unwanted lines
    Adds new coordinates, remove extra for loops, optimizes ortho call, indents

--- BE/VTU/6/lab/CGV/1.c
+++ BE/VTU/6/lab/CGV/1.c
@@ -2,13 +2,16 @@
 #include<stdio.h>
 #include<GL/glut.h>
 typedef float point[3];
-point v[] = {{0.0,0.0,1.0},
-	{0.0,0.942809,-0.33333},
-	{-0.81697,-0.471405,-0.333333},
-	{0.816497,-0.471405,-0.33333}};
-int n;
 
-void triangle(point a,point b,point c)
+int n;
+point v[] = {
+	{0.0,0.0,1.0},
+	{0.0,1.0,0.0},
+	{-1.0,-0.7,0.0},
+	{1.0,-0.7,0.0}
+};
+
+void triangle(point a, point b, point c)
 {
 	glBegin(GL_POLYGON);
 	glVertex3fv(a);
@@ -17,24 +20,22 @@ void triangle(point a,point b,point c)
 	glEnd();
 }
 
-void divide_triangle(point a,point b,point c,int m)
+void divide_triangle(point a, point b, point c, int m)
 {
 	point v1,v2,v3;
 	int j;
 	if(m>0)
 	{
 		for(j=0;j<3;j++)
+		{			
 			v1[j]=(a[j]+b[j])/2;
-		for(j=0;j<3;j++)
 			v2[j]=(a[j]+c[j])/2;
-		for(j=0;j<3;j++)
 			v3[j]=(b[j]+c[j])/2;
-
+		}
 		divide_triangle(a,v1,v2,m-1);
 		divide_triangle(c,v2,v3,m-1);
 		divide_triangle(b,v3,v1,m-1);
 	}
-
 	else triangle(a,b,c);
 }
 
@@ -43,11 +44,11 @@ void tetrahedron(int m)
 	glColor3f(1.0,0.0,0.0);
 	divide_triangle(v[0],v[1],v[2],m);
 	glColor3f(0.0,1.0,0.0);
-	divide_triangle(v[3],v[2],v[1],m);
+	divide_triangle(v[1],v[2],v[3],m);
 	glColor3f(0.0,0.0,1.0);
-	divide_triangle(v[0],v[3],v[1],m);
-	glColor3f(0.0,0.0,0.0);
-	divide_triangle(v[0],v[2],v[3],m);
+	divide_triangle(v[2],v[3],v[0],m);
+	glColor3f(1.0,1.0,0.0);
+	divide_triangle(v[3],v[0],v[1],m);
 }
 
 void display(void)
@@ -58,26 +59,24 @@ void display(void)
 	glFlush();
 }
 
-void myReshape(int w,int h)
+void myReshape(int w, int h)
 {
 	glViewport(0,0,w,h);
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
 	if(w<=h)
-
 		glOrtho(-2.0,2.0,-2.0*(GLfloat)h/(GLfloat)w,2.0*(GLfloat)h/(GLfloat)w,-10.0,10.0);
 	else
-		glOrtho(-2.0*(GLfloat)w/(GLfloat)h,2.0*(GLfloat)w/(GLfloat)h,-2.0,2.0,-10.0,10.0);
-
+		glOrtho(-2.0,2.0,-2.0*(GLfloat)w/(GLfloat)h,2.0*(GLfloat)w/(GLfloat)h,-10.0,10.0);
 	glMatrixMode(GL_MODELVIEW);
 	glutPostRedisplay();
 }
 
-int main(int argc,char **argv)
+int main(int argc, char **argv)
 {
 	printf("number of divisions \n");
 	scanf("%d",&n);
-	glutInit(&argc,argv);
+	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_SINGLE|GLUT_RGB|GLUT_DEPTH);
 	glutInitWindowSize(500,500);
 	glutCreateWindow("3Dgasket");
